d2483c684ca553d03ec8b97e82ebe1116c891acc
Romildo Paiter <romildo.paiter@amedigital.com>
2024-04-16 21:55:22 -0300

    alter to mensage sifrada

--- atv02/atv02-ex4.py
+++ atv02/atv02-ex4.py
@@ -1,15 +1,15 @@
 
 def altera_nos_pares(caracter, mensagem = "", qtd_caracter_mensagem = 0):    
-    nova_sifrada = ""
+    mensagem_sifrada = ""
     c = 0
     while c < qtd_caracter_mensagem:
         if (c % 2 == 0):        
-            nova_sifrada = nova_sifrada + caracter
+            mensagem_sifrada = mensagem_sifrada + caracter
         else:
-            nova_sifrada = nova_sifrada + mensagem[c]
+            mensagem_sifrada = mensagem_sifrada + mensagem[c]
         c += 1
         
-    return nova_sifrada
+    return mensagem_sifrada
     
 mensagem = "Aulas de Python daquele jeito"
 nova_mensagem = altera_nos_pares("&", mensagem, len(mensagem))
